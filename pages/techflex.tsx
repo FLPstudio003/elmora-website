226a70b986160aabe4754ba4c28b912f1ffbbec8
FLPstudio003 <graphicsflp@gmail.com>
2026-06-18 18:57:19 +0200

    Update techflex.tsx

--- pages/techflex.tsx
+++ pages/techflex.tsx
@@ -106,7 +106,7 @@ export default function Techflex() {
         <section className="px-6 pb-24 text-center">
           <div className="flex justify-center gap-6 mt-6">
 
-            
+            <a
               href="/home#contact"
               className="bg-[#01382E] text-white px-8 py-3 rounded-full hover:bg-[#012b23] transition"
             >
